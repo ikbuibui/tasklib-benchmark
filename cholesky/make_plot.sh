2d8acbcba7ba5d6bf6c6ffe1e1b2c39905356c3c
Michael Sippel <m.sippel@hzdr.de>
2022-07-14 14:31:32 +0200

    cholesky: adapt make_plot.sh

--- cholesky/make_plot.sh
+++ cholesky/make_plot.sh
@@ -6,21 +6,23 @@ cmake ..
 make -j
 cd ..
 
-n_repeat=5
+n_repeat=1
 n_workers=4
-delay=10
+matrix_size=4096
 
 for lib in redgrapes superglue quark;
 do
     truncate -s 0 ${lib}_data
-    for dim in 4 8 16 32 64;
+    for nblks in 1 2 4 8 16 32 64;
     do
 	DATA=""
 
+        blksz=$((matrix_size / nblks))
+
 	for i in $(seq $n_repeat);
 	do
-	    echo $lib $dim $delay $n_workers
-	    OUTPUT=$(numactl -C 0-$((n_workers - 1)) ./build/$lib $dim $delay $n_workers)
+	    echo $lib $blksz $nblks $n_workers
+	    OUTPUT=$(numactl -C 0-$((n_workers - 1)) ./build/$lib $blksz $nblks $n_workers)
 	    TOTAL=$(echo $OUTPUT | grep -Po 'total \K[0-9.]*')
 	    DATA="$TOTAL $DATA"
 	done
@@ -31,17 +33,17 @@ do
 	AVG=$(awk '{sum = 0; for(i=1;i<=NF;i++) { sum += $i } } END { printf "%f\n", sum/NF }' <<< $DATA)
 
 	echo "min=$MIN, max=$MAX, avg=$AVG"
-	echo "$dim $AVG $MIN $MAX" >> ${lib}_data
+	echo "$nblks $AVG $MIN $MAX" >> ${lib}_data
     done
 done
 
-OUTPUT="bench_cholesky_w${n_workers}_d${delay}.png"
+OUTPUT="bench_cholesky_w${n_workers}_m${matrix_size}.png"
 
 gnuplot -p \
    -e "set output \"${OUTPUT}\"" \
    -e 'set terminal png size 800,600 enhanced font "Computer Modern,16"' \
-   -e "set title \"cholesky factorization\n $delay μs task duration,\n $n_workers workers\"" \
-   -e 'set xlabel "matrix size (#tiles^{0.5})"' \
+   -e "set title \"cholesky factorization\n $matrix_size x $matrix_size matrix\n $n_workers workers\"" \
+   -e 'set xlabel "tiling factor (#tiles^{0.5})"' \
    -e 'set ylabel "runtime (ms)"' \
    -e 'set key right top' \
    -e 'set grid' \
