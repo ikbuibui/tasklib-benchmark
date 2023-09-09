4812644666c1f85d86bfb1eba7f1334ef9b2abc2
Michael Sippel <m.sippel@hzdr.de>
2023-09-09 13:00:08 +0200

    fix install quarks install prefix

--- share/build_quark.sh
+++ share/build_quark.sh
@@ -2,8 +2,8 @@
 
 curl https://icl.utk.edu/projectsfiles/quark/pubs/quark-0.9.0.tgz | tar xfz -
 cd quark-0.9.0
-sed -i -e 's/CC=.*/CC=gcc/' make.inc
-sed -i -e 's/CFLAGS=-O2/CFLAGS=-O3/' make.inc
-make -j
-prefix=${BENCHMARK_DIR}/thirdparty/.install make install
+sed -i -e 's~^CC=.*$~CC=gcc~' make.inc
+sed -i -e 's~^CFLAGS=-O2$~CFLAGS=-O3~' make.inc
+sed -i -e "s~^prefix=.*$~prefix=${BENCHMARK_DIR}/thirdparty/.install~" make.inc
+make -j install
 
