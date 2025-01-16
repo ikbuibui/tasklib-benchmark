11a91ecb6edbfe091f34f24cc79b19e80deb5992
Tapish <narwal83@hzdr.de>
2025-01-16 10:34:29 +0100

    Do a big scaling test

--- run_all.sh
+++ run_all.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-export n_workers=64
-export n_resources=$n_workers
+worker_counts=(1 2 4 8 16 32 64) # Worker counts
+export n_resources=64  # Assuming n_resources stays constant
 export n_repeat=15
 
 README="$(pwd)/README.md"
@@ -12,62 +12,65 @@ update_image() {
     sed -i "s~\[$1\]([^\)]*)~[$1]($2)~g" ${README}
 }
 
-pushd latency
-. ./make_plot.sh
-echo "OUTPUT=${OUTPUT}"
-update_image "Latency" "latency/${OUTPUT}"
-popd
+for n_workers in "${worker_counts[@]}"; do
+    export n_workers
+    echo "Running for n_workers=${n_workers}..."
 
-pushd randomGraph
-. ./scripts/bench_emplacement.sh
-update_image "Emplacement" "randomGraph/${OUTPUT}"
+    pushd latency
+    . ./make_plot.sh
+    echo "OUTPUT=${OUTPUT}"
+    update_image "Latency (Workers: ${n_workers})" "latency/${OUTPUT}"
+    popd
 
-export min_task_duration=200
-export max_task_duration=200
+    pushd randomGraph
+    . ./scripts/bench_emplacement.sh
+    update_image "Emplacement (Workers: ${n_workers})" "randomGraph/${OUTPUT}"
 
-export min_dependencies=0
-export max_dependencies=0
-. ./scripts/bench_overhead.sh
-update_image "Overhead: Independent" "randomGraph/${OUTPUT}"
+    export min_task_duration=200
+    export max_task_duration=200
 
-export min_dependencies=1
-export max_dependencies=1
-. ./scripts/bench_overhead.sh
-update_image "Overhead: Chains" "randomGraph/${OUTPUT}"
+    export min_dependencies=0
+    export max_dependencies=0
+    . ./scripts/bench_overhead.sh
+    update_image "Overhead: Independent (Workers: ${n_workers})" "randomGraph/${OUTPUT}"
 
-export min_dependencies=1
-export max_dependencies=5
-. ./scripts/bench_overhead.sh
-update_image "Overhead: Random" "randomGraph/${OUTPUT}"
+    export min_dependencies=1
+    export max_dependencies=1
+    . ./scripts/bench_overhead.sh
+    update_image "Overhead: Chains (Workers: ${n_workers})" "randomGraph/${OUTPUT}"
+
+    export min_dependencies=1
+    export max_dependencies=5
+    . ./scripts/bench_overhead.sh
+    update_image "Overhead: Random (Workers: ${n_workers})" "randomGraph/${OUTPUT}"
+
+    export min_task_duration=0
+    export max_task_duration=0
+    . ./scripts/bench_scheduling_gap.sh
+    update_image "Scheduling Gap: 0μs (Workers: ${n_workers})" "randomGraph/$OUTPUT"
+
+    export min_task_duration=50
+    export max_task_duration=50
+    . ./scripts/bench_scheduling_gap.sh
+    update_image "Scheduling Gap: 50μs (Workers: ${n_workers})" "randomGraph/$OUTPUT"
+
+    export min_task_duration=25
+    export max_task_duration=500
+    . ./scripts/bench_scheduling_gap.sh
+    update_image "Scheduling Gap: 25-500μs (Workers: ${n_workers})" "randomGraph/$OUTPUT"
+
+    popd
 
 
+    # pushd cholesky
 
+    # matrix_size=4096 . ./make_plot.sh
+    # update_image "Cholesky 128MiB (Workers: ${n_workers})" "cholesky/${OUTPUT}"
 
-export min_task_duration=0
-export max_task_duration=0
-. ./scripts/bench_scheduling_gap.sh
-update_image "Scheduling Gap: 0μs" "randomGraph/$OUTPUT"
+    # matrix_size=8192 . ./make_plot.sh
+    # update_image "Cholesky 512MiB (Workers: ${n_workers})" "cholesky/${OUTPUT}"
 
-export min_task_duration=50
-export max_task_duration=50
-. ./scripts/bench_scheduling_gap.sh
-update_image "Scheduling Gap: 50μs" "randomGraph/$OUTPUT"
+    # popd
 
-export min_task_duration=25
-export max_task_duration=500
-. ./scripts/bench_scheduling_gap.sh
-update_image "Scheduling Gap: 25-500μs" "randomGraph/$OUTPUT"
-
-popd
-
-
-# pushd cholesky
-
-# matrix_size=4096 . ./make_plot.sh
-# update_image "Cholesky 128MiB" "cholesky/${OUTPUT}"
-
-# matrix_size=8192 . ./make_plot.sh
-# update_image "Cholesky 512MiB" "cholesky/${OUTPUT}"
-
-# popd
+done
 
